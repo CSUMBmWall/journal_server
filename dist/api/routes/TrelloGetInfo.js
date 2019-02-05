44ef5672330bfa0789a5786f78a8b8eb808ca67a
Matt <mswall77@hotmail.com>
2019-02-05 08:46:42 -0800

    added more trello stuff

--- dist/api/routes/TrelloGetInfo.js
+++ dist/api/routes/TrelloGetInfo.js
@@ -8,7 +8,7 @@ const trelloKey = process.env.TRELLO_KEY;
 const trelloToken = process.env.TRELLO_TOKEN;
 const trelloPostFix = 'key=' + trelloKey + '&token=' + trelloToken;
 const getBoardsURL = 'https://api.trello.com/1/members/me/boards?key=' + trelloKey + '&token=' + trelloToken;
-const getCardsURL = 'https://api.trello.com/1/cards';
+var getCardsURL = 'https://api.trello.com/1/boards/';
 // fix any types -- add class for expected data and find out what type request.body is
 router.post('/getBoards', (req, res, next) => {
     request.get(getBoardsURL, (err, response, body) => {
@@ -29,10 +29,53 @@ router.post('/getBoards', (req, res, next) => {
         }
     });
 });
+// router.post('/getCards', (req: Request, res: Response, next: any) => {
+//     console.log('id ' + JSON.stringify(req.body));
+//     getCardsURL += req.body.id + '/cards/?' + trelloPostFix;
+//     console.log(this.getCardsURL);
+//     request.get(getCardsURL, (err: Error, response: Response, body: any) => {
+//         if (err) {
+//             res.status(500).json({
+//                 error: err
+//             })
+//         } else {
+//             // var card_list = [];
+//             // for (const card of JSON.parse(body)) {
+//             //     card_list.push({
+//             //         name: card.name,
+//             //         id: card.id
+//             //     });
+//             // }
+//             var cards = [];
+//             for (const item of JSON.parse(body)) {
+//                 // console.log(item);
+//                 console.log('name', item.name);
+//                 console.log('labels', item.labels);
+//             }
+//             res.status(200).send(body);
+//         }
+//     });
+//     // res.status(200).json({msg: 'all_good'});
+// });
 router.post('/getCards', (req, res, next) => {
-    console.log('id ' + JSON.stringify(req.body));
-    console.log('id ' + 'k');
-    // this.getCardsURL += req.body.id + trelloPostFix;
+    var request = require("request");
+    var options = { method: 'GET',
+        url: 'https://api.trello.com/1/boards/' + req.body.id + '/lists',
+        qs: { cards: 'all',
+            card_fields: 'all',
+            filter: 'open',
+            fields: 'all',
+            key: trelloKey,
+            token: trelloToken } };
+    request(options, function (err, response, body) {
+        if (err)
+            console.log(err);
+        console.log(body);
+        res.status(200).send(body);
+    });
+    // console.log('id ' + JSON.stringify(req.body));
+    // getCardsURL += req.body.id + '/cards/?' + trelloPostFix;
+    // console.log(this.getCardsURL);
     // request.get(getCardsURL, (err: Error, response: Response, body: any) => {
     //     if (err) {
     //         res.status(500).json({
@@ -46,10 +89,16 @@ router.post('/getCards', (req, res, next) => {
     //         //         id: card.id
     //         //     });
     //         // }
+    //         var cards = [];
+    //         for (const item of JSON.parse(body)) {
+    //             // console.log(item);
+    //             console.log('name', item.name);
+    //             console.log('labels', item.labels);
+    //         }
     //         res.status(200).send(body);
     //     }
     // });
-    res.status(200).json({ msg: 'all_good' });
+    // // res.status(200).json({msg: 'all_good'});
 });
 module.exports = router;
 //# sourceMappingURL=TrelloGetInfo.js.map
--- src/api/routes/TrelloGetInfo.ts
+++ src/api/routes/TrelloGetInfo.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from "express";
+import {error} from "util";
 
 const express = require('express');
 const router = express.Router();
@@ -10,7 +11,7 @@ const trelloKey = process.env.TRELLO_KEY;
 const trelloToken = process.env.TRELLO_TOKEN;
 const trelloPostFix = 'key=' + trelloKey + '&token=' + trelloToken;
 const getBoardsURL = 'https://api.trello.com/1/members/me/boards?key=' + trelloKey + '&token=' + trelloToken;
-const getCardsURL = 'https://api.trello.com/1/cards';
+var getCardsURL = 'https://api.trello.com/1/boards/';
 
 
 // fix any types -- add class for expected data and find out what type request.body is
@@ -34,10 +35,58 @@ router.post('/getBoards', (req: any, res: Response, next: any) => {
 
 });
 
+// router.post('/getCards', (req: Request, res: Response, next: any) => {
+//     console.log('id ' + JSON.stringify(req.body));
+//     getCardsURL += req.body.id + '/cards/?' + trelloPostFix;
+//     console.log(this.getCardsURL);
+//     request.get(getCardsURL, (err: Error, response: Response, body: any) => {
+//         if (err) {
+//             res.status(500).json({
+//                 error: err
+//             })
+//         } else {
+//             // var card_list = [];
+//             // for (const card of JSON.parse(body)) {
+//             //     card_list.push({
+//             //         name: card.name,
+//             //         id: card.id
+//             //     });
+//             // }
+//             var cards = [];
+//             for (const item of JSON.parse(body)) {
+//                 // console.log(item);
+//                 console.log('name', item.name);
+//                 console.log('labels', item.labels);
+//             }
+//             res.status(200).send(body);
+//         }
+//     });
+//     // res.status(200).json({msg: 'all_good'});
+// });
+
 router.post('/getCards', (req: Request, res: Response, next: any) => {
-    console.log('id ' + JSON.stringify(req.body));
-    console.log('id ' + 'k');
-    // this.getCardsURL += req.body.id + trelloPostFix;
+    var request = require("request");
+
+    var options = { method: 'GET',
+        url: 'https://api.trello.com/1/boards/'+ req.body.id + '/lists',
+        qs:
+            { cards: 'all',
+                card_fields: 'all',
+                filter: 'open',
+                fields: 'all',
+                key: trelloKey,
+                token: trelloToken } };
+
+    request(options, function (err: Error, response: Response, body: any) {
+        if (err) console.log(err);
+
+        console.log(body);
+        res.status(200).send(body);
+    });
+
+    // console.log('id ' + JSON.stringify(req.body));
+    // getCardsURL += req.body.id + '/cards/?' + trelloPostFix;
+    // console.log(this.getCardsURL);
     // request.get(getCardsURL, (err: Error, response: Response, body: any) => {
     //     if (err) {
     //         res.status(500).json({
@@ -51,10 +100,17 @@ router.post('/getCards', (req: Request, res: Response, next: any) => {
     //         //         id: card.id
     //         //     });
     //         // }
+    //         var cards = [];
+    //         for (const item of JSON.parse(body)) {
+    //             // console.log(item);
+    //             console.log('name', item.name);
+    //             console.log('labels', item.labels);
+    //         }
     //         res.status(200).send(body);
     //     }
     // });
-    res.status(200).json({msg: 'all_good'});
+    // // res.status(200).json({msg: 'all_good'});
 });
-
 module.exports = router;
+
+
